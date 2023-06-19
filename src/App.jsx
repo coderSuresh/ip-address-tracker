2f9ff5ac03c8196d2cd3680debfebd375814cc79
codersuresh <sd807141@gmail.com>
2023-06-19 16:52:20 +0545

    make details card responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import Header from './components/Header'
 const Details = () => {
   return (
     <div className="mx-5">
-      <div className='flex gap-5 bg-white shadow-sm rounded-xl px-10 py-6 justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
+      <div className='flex flex-col text-center md:text-start items-center md:flex-row gap-5 bg-white shadow rounded-xl px-10 py-6 md:items-start justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
         <div className="ip flex-1">
           <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>IP ADDRESS</p>
           <p className="text-xl text-very-dark-gray font-medium">192.168.1.10</p>
         </div>
 
-        <div className="divider my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
+        <div className="divider md:block hidden my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
 
         <div className="location flex-1">
           <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>LOCATION</p>
           <p className="text-xl text-very-dark-gray font-medium">Brooklyn, NY 10001</p>
         </div>
 
-        <div className="divider my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
+        <div className="divider md:block hidden my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
 
         <div className="timezone flex-1 ">
           <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>TIMEZONE</p>
           <p className="text-xl text-very-dark-gray font-medium">UTC-05:00</p>
         </div>
 
-        <div className="divider my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
+        <div className="divider md:block hidden my-3 ml-5 w-[1px] opacity-40 bg-dark-gray" />
 
         <div className="isp flex-1">
           <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>ISP</p>
