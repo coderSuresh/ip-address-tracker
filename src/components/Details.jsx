118e688257db3a3be96469e72b72ea460cfcc65f
codersuresh <sd807141@gmail.com>
2023-08-03 07:15:30 +0545

    feat: show dummy data while loading

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -24,39 +24,35 @@ const Details = () => {
 
     return (
         <div className="mx-5">
-            {
-                loading ? <h1>Loading...</h1> :
-                    <div className='flex flex-col text-center md:text-start items-center md:flex-row gap-5 bg-white shadow rounded-xl px-10 py-6 md:items-start justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
-                        <div className="ip flex-1">
-                            <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>IP ADDRESS</p>
-                            <p className="text-xl text-very-dark-gray font-medium">{ip}</p>
-                        </div>
+            <div className='flex flex-col text-center md:text-start items-center md:flex-row gap-5 bg-white shadow rounded-xl px-10 py-6 md:items-start justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
+                <div className="ip flex-1">
+                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>IP ADDRESS</p>
+                    <p className="text-xl text-very-dark-gray font-medium">{ loading ? '192.212.174.101': ip  }</p>
+                </div>
 
-                        <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                        <div className="location flex-1">
-                            <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>LOCATION</p>
-                            <p className="text-xl text-very-dark-gray font-medium">{location}</p>
-                        </div>
+                <div className="location flex-1">
+                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>LOCATION</p>
+                    <p className="text-xl text-very-dark-gray font-medium">{ loading ? 'Brooklyn, NY, 10001' : location }</p>
+                </div>
 
-                        <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                        <div className="timezone flex-1 ">
-                            <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>TIMEZONE</p>
-                            <p className="text-xl text-very-dark-gray font-medium">{timezone}</p>
-                        </div>
+                <div className="timezone flex-1 ">
+                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>TIMEZONE</p>
+                    <p className="text-xl text-very-dark-gray font-medium">{loading ? 'UTC-05:00' : timezone}</p>
+                </div>
 
-                        <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                        <div className="isp flex-1">
-                            <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>ISP</p>
-                            <p className="text-xl text-very-dark-gray font-medium">{isp}</p>
-                        </div>
-                    </div>
-            }
+                <div className="isp flex-1">
+                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>ISP</p>
+                    <p className="text-xl text-very-dark-gray font-medium">{loading ? 'SpaceX Starlink' : isp}</p>
+                </div>
+            </div>
         </div>
     )
 }
-
 
 export default Details
