ba93a59fb0e4e6ab70fb89f6b441394f9825549b
codersuresh <sd807141@gmail.com>
2023-08-04 07:53:08 +0545

    feat: show info and location on load

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
+import MapComponent from './MapComponent'
 
 const Details = () => {
     const [ip, setIp] = useState('')
     const [location, setLocation] = useState('')
     const [timezone, setTimezone] = useState('')
+    const [latLong, setLatLong] = useState({
+        latitude: 0,
+        longitude: 0
+    })
     const [isp, setIsp] = useState('')
     const [loading, setLoading] = useState(true)
 
@@ -15,6 +20,11 @@ const Details = () => {
         setLocation(`${data.city}, ${data.region}, ${data.country_name}`)
         setTimezone(data.timezone)
         setIsp(data.org)
+        setLatLong({
+            latitude: parseFloat(data.latitude),
+            longitude: parseFloat(data.longitude)
+        })
+        
         setLoading(false)
     }
 
@@ -23,35 +33,40 @@ const Details = () => {
     }, [])
 
     return (
-        <div className="mx-5">
-            <div className='flex flex-col text-center md:text-start items-center md:flex-row gap-5 bg-white shadow rounded-xl px-10 py-6 md:items-start justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
-                <div className="ip flex-1">
-                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>IP ADDRESS</p>
-                    <p className="text-xl text-very-dark-gray font-medium">{ loading ? '192.212.174.101': ip  }</p>
-                </div>
+        <>
+            <div className="mx-5">
+                <div className='flex flex-col text-center md:text-start items-center md:flex-row gap-5 bg-white shadow rounded-xl px-10 py-6 md:items-start justify-between md:mx-auto mt-[-4rem] md:max-w-4xl w-full'>
+                    <div className="ip flex-1">
+                        <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>IP ADDRESS</p>
+                        <p className="text-xl text-very-dark-gray font-medium">{loading ? '192.212.174.101' : ip}</p>
+                    </div>
 
-                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                    <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                <div className="location flex-1">
-                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>LOCATION</p>
-                    <p className="text-xl text-very-dark-gray font-medium">{ loading ? 'Brooklyn, NY, 10001' : location }</p>
-                </div>
+                    <div className="location flex-1">
+                        <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>LOCATION</p>
+                        <p className="text-xl text-very-dark-gray font-medium">{loading ? 'Brooklyn, NY, 10001' : location}</p>
+                    </div>
 
-                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                    <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                <div className="timezone flex-1 ">
-                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>TIMEZONE</p>
-                    <p className="text-xl text-very-dark-gray font-medium">{loading ? 'UTC-05:00' : timezone}</p>
-                </div>
+                    <div className="timezone flex-1 ">
+                        <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>TIMEZONE</p>
+                        <p className="text-xl text-very-dark-gray font-medium">{loading ? 'UTC-05:00' : timezone}</p>
+                    </div>
 
-                <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
+                    <div className="divider md:block hidden my-3 ml-5 h-16 w-[1px] opacity-40 bg-dark-gray" />
 
-                <div className="isp flex-1">
-                    <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>ISP</p>
-                    <p className="text-xl text-very-dark-gray font-medium">{loading ? 'SpaceX Starlink' : isp}</p>
+                    <div className="isp flex-1">
+                        <p className='text-xs text-dark-gray uppercase tracking-widest font-medium mb-2'>ISP</p>
+                        <p className="text-xl text-very-dark-gray font-medium">{loading ? 'SpaceX Starlink' : isp}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            {
+                !loading && <MapComponent location={location} latLong={latLong} />
+           }
+        </>
     )
 }
 
---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapComponent = ({ location, latLong }) => {
+
+    const [position, setPosition] = React.useState([latLong.latitude, latLong.longitude]);
+
+    React.useEffect(() => {
+        setPosition([latLong.latitude, latLong.longitude]);
+    }, [latLong]);
+
+    return (
+        <div className='absolute top-52 left-0 right-0 z-[-1] w-full h-[calc(100vh-208px)]'>
+            <MapContainer
+                center={position}
+                zoom={13}
+                zoomControl={false}
+                style={
+                    {
+                        height: '100%',
+                        width: '100%'
+                    }
+                }
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        {location}
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+};
+
+export default MapComponent;
